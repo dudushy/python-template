ae6c07998800b4106ce113c5a905744604f19ff5
Eduardo Talarico <eduardotalarico1337@gmail.com>
2023-05-29 20:08:08 -0300

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-# pip freeze | xargs pip uninstall -y
-# pip freeze > requirements.txt
-#* -- Imports
-import os
-
-os.system("pip install -r requirements.txt") #? Install requirements
-os.system("pause") #? Send "pause" to system (Press any key to continue . . .)
